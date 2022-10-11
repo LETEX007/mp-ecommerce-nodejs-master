b3412195517f5910737ccd07d1610e62d3b0d0f3
lete <103456882+LETEX007@users.noreply.github.com>
2022-10-11 12:32:29 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var port = process.env.PORT || 3000
+
+var app = express();
+ 
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('assets'));
+ 
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/detail', function (req, res) {
+    res.render('detail', req.query);
+});
+app.post('/pago', function (req, res) {
+
+    // res.render('pago', req.query);
+});
+
+app.listen(port);
