c30e47ceb4db057ac4776d6607ea4922b810dc65
lete <103456882+LETEX007@users.noreply.github.com>
2022-10-11 21:31:13 -0300

    Actualizacion SDK Checkout Pro

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.set('view engine', 'handlebars');
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
+const mercadopago = require("mercadopago");
+
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+	access_token: "APP_USR-8709825494258279-092911-227a84b3ec8d8b30fff364888abeb67a-1160706432",
+	
+	integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
+});
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -18,9 +26,67 @@ app.get('/', function (req, res) {
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
-app.post('/pago', function (req, res) {
+app.post("/create_preference", (req, res) => {
 
-    // res.render('pago', req.query);
+	let preference = {
+		payer: {
+			name: "Lalo",
+			surname: "Landa",
+			email: "test_user_36961754@testuser.com",
+			phone: {
+				area_code: "56",
+				number: 937485958
+			},
+			address: {
+				street_name: "calle falsa",
+				street_number: 123,
+				zip_code: "2490000"
+			}
+		},
+		items: [
+			{
+				id:req.body.idprod,
+				title: req.body.title,
+				description:"Dispositivo móvil de Tienda e-commerce",
+				picture_url: req.body.img,
+				quantity: Number(Number(req.body.quantity)),
+				unit_price: Number(Number(req.body.unit_price)),
+
+			}
+		],
+		back_urls: {
+			"success": "http://localhost:8080/notificacion",
+			"failure": "http://localhost:8080/notificacion",
+			"pending": "http://localhost:8080/notificacion"
+		},
+		auto_return: "approved",
+		payment_methods: {
+			excluded_payment_methods: [
+				{
+					id: "visa"
+				}
+			],
+			installments: 6
+		},
+		notification_url: "https://localhost:8080/notificacion",
+		external_reference: "rorolete02@gmail.com",
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+app.get('/notificacion', function(req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
 });
 
 app.listen(port);
---
+++ assets/detail.js
@@ -0,0 +1,59 @@
+// Add SDK credentials
+// REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
+const mercadopago = new MercadoPago('APP_USR-ff96fe80-6866-4888-847e-c69250754d38', {
+    locale: 'es-AR' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
+  });
+  
+  // Handle call to backend and generate preference.
+  document.getElementById("btnPago").addEventListener("click", function() {
+  
+    $('#btnPago').attr("disabled", true);
+    const orderData = {
+        idprod:randomID(),
+        title: document.getElementById("title").innerHTML,
+        quantity: document.getElementById("quantity").innerHTML,
+        unit_price: document.getElementById("unit_price").innerHTML,
+        picture_url: document.getElementById("img").value
+    };
+      
+    fetch("http://localhost:8080/create_preference", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(orderData),
+    })
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(preference) {
+          createCheckoutButton(preference.id);
+          
+          $(".as-accessories-results").fadeOut(500);
+          setTimeout(() => {
+              $(".as-producttile-info").show(500).fadeIn();
+          }, 500);
+      })
+      .catch(function() {
+          alert("Unexpected error");
+          $('#btnPago').attr("disabled", false);
+      });
+  });
+  
+  // Create preference when click on checkout button
+  function createCheckoutButton(preferenceId) {
+    // Initialize the checkout
+    mercadopago.checkout({
+      preference: {
+        id: preferenceId
+      },
+      render: {
+        container: '#button-checkout', // Class name where the payment button will be displayed
+        label: 'Pagar la compra', // Change the payment button text (optional)
+      }
+    });
+  }
+  function randomID(){
+    return Math.floor(Math.random() * 9000) + 1000; // Retonar un numero entre 1000 y 9999
+  }
+  
