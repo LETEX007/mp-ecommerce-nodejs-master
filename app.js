7c9d4b63bf91ca3dba888526501023a82ad3ebd7
lete <103456882+LETEX007@users.noreply.github.com>
2022-11-01 03:02:34 -0300

    test

--- app.js
+++ app.js
@@ -22,9 +22,22 @@ mercadopago.configure({
 	integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
 });
 
-
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
+	res.setHeader('Cross-origin-Opener-Policy','same-origin');
   
+	if (req.method === 'OPTIONS') {
+	  res.sendStatus(200)
+	} else {
+	  next()
+	}
+  });
 app.get('/', function (req, res) {
+	res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
+	res.setHeader('Cross-origin-Opener-Policy','same-origin');
     res.render('home');
 });
 
